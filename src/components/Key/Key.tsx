3e7a346ce41f48f0a07c060b213ad7fd9e78504d
shutsugan <shutsugan@gmail.com>
2020-08-01 22:00:05 +0100

    feat(key): implement keyboard

---
+++ src/components/Key/Key.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+import clsx from "clsx";
+import { NoteType } from "../../domain/note";
+
+interface KeyProps {
+  type: NoteType;
+  label: string;
+  disabled?: boolean;
+}
+
+export const Key: FunctionComponent<KeyProps> = (props) => {
+  const { type, label, ...rest } = props;
+
+  return (
+    <button className={clsx(`key key--${type}`)} type="button" {...rest}>
+      {label}
+    </button>
+  );
+};
