d3157975ee0f7144669fd742bee8f4a022fdf471
shutsugan <shutsugan@gmail.com>
2020-08-10 20:06:28 +0100

    refactor and clean code

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,5 @@
 import React, { FunctionComponent } from "react";
 
-import { KeyboardWithInstrument } from "../KeyBoard/WithInstruments";
 import { NoAudioMessage } from "../NoAudioMessage";
-import { useAudioContext } from "../AudioContextProvider";
 
-export const Main: FunctionComponent = () => {
-  const audioContext = useAudioContext();
-
-  return !!audioContext ? <KeyboardWithInstrument /> : <NoAudioMessage />;
-};
+export const Main: FunctionComponent = () => <NoAudioMessage />;
